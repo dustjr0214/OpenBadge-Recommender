90890c8bcafe137808885955d16c5b434f1b56dd
dustjr0214 <yslee5772@gmail.com>
2025-06-14 03:27:17 +0900

    render 배포용 설정 수정

--- src/web/main.py
+++ src/web/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.web.route import recommendation
+import uvicorn
+import os
 
 app = FastAPI(
     title="OpenBadge Recommendation API",
@@ -11,7 +13,12 @@ app = FastAPI(
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 실제 운영 환경에서는 특정 도메인만 허용하도록 수정
+    allow_origins=["http://localhost:3000",
+                   "http://localhost:8000",
+                   "https://openbadgesyu.web.app/",
+                   "https://openbadgesyu.firebaseapp.com/",
+                   "https://*.onrender.com",
+                   ],  # 실제 운영 환경에서는 특정 도메인만 허용하도록 수정
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -30,3 +37,7 @@ async def root():
         "version": "1.0.0",
         "docs_url": "/docs"
     } 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
